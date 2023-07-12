68f1122176b5a844182c5bc4d715d8b10f61d1d1
Anton <xxahtoxaxx2015@gmail.com>
2023-07-12 21:37:19 +0400

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Антон Исаков on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
